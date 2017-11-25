c251fcdc2c23129809bc66b008c3dc07b47e283f
Mason Fischer <mason@kissr.co>
2017-11-25 13:24:58 -0800

    Initial commit 🍔

---
+++ contracts/HamburgerCoin.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+import "zeppelin-solidity/contracts/token/StandardToken.sol";
+
+contract HamburgerCoin is StandardToken {
+  string public name = "HamburgerCoin"; 
+  string public symbol = "HBC";
+  uint public decimals = 2;
+  uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
+
+  function HamburgerCoin() public {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
+
